d960bf83f6611b7ed5de146e94a9f990c10e41cb
Eldar Terzic <eldar.terzic@insum.se>
2022-12-10 23:25:49 +0100

    Init

--- Synonyms/Program.cs
+++ Synonyms/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GroupDocs.Search;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Type a word for which you want to find a synonym for:");
+        string query = Console.ReadLine();
+
+        string[] synonyms = new GroupDocs.Search.Index().Dictionaries.SynonymDictionary.GetSynonyms(query);
+
+
+        if (synonyms.Length > 0)
+        {
+            Console.WriteLine("\"List of synonyms for '" + query + "':\"");
+            Console.WriteLine("-----------------------------------------");
+            for (int i = 0; i < synonyms.Length; i++)
+            {
+                Console.WriteLine("- " + synonyms[i]);
+            }
+            Console.WriteLine("-----------------------------------------");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunaly we can't find any synonymes for, " + query + ".");
+        }
+    }
+}
